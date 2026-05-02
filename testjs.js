d0357e8cfe220bf6a8c2c1b74501e5900daff3c7
Ivan89183924788 <ivorotnik@yandex.ru>
2026-05-02 19:53:16 +0300

    first commit

---
+++ testjs.js
@@ -0,0 +1,1141 @@
+/*let num=prompt
+alert(num==undefined)
+
+let message;
+message='Hello'
+alert(message)
+
+let message='Hello';
+alert(message)
+
+var message='Hello';
+alert(message)
+
+const myBirthday='19.04.1985'
+alert(myBirthday)
+
+const myBirthday='19.04.1985'
+myBirthday='23.12.1990'
+alert(myBirthday)
+
+let age=23
+age=12
+alert(age)
+
+let name='djon'
+let admin=name
+alert(admin)
+
+alert(1/0)
+
+alert("not number"/2)
+
+alert(10)
+
+const bigInt = 123456789012345678901234567890123456789089;
+alert(bigInt)
+
+const bigInt = 1234567890123456789012345678901234567890n;
+alert(bigInt)
+
+let message='hello'
+alert(message)
+
+let name='Иван';
+alert(`Привет,${name}`);
+
+alert(`результат:${1+2}`);
+
+let age;
+alert(age);
+
+let age1;
+alert(age1);
+alert(typeof(age1));
+
+let age2=35;
+alert(age2);
+alert(typeof(age2));
+
+let name='Aboba'
+alert(`Hello ${name}`)
+alert(typeof(name));
+
+
+let age=('number'/2)
+alert(age)
+alert(typeof(age));
+
+const bigInt = 1234567890123456789012345678901234567890n;
+alert(bigInt)
+alert(typeof(bigInt))
+
+let age1=null
+alert(age1)
+alert(typeof(age1))
+
+let age2=true;
+alert(typeof(age2));
+
+alert(typeof(null))
+
+alert(typeof(1999));
+
+alert(typeof('string radosti'))
+
+alert(typeof(true))
+
+alert(typeof('qwwww'/2))
+
+alert(typeof(1234567890123456789012345678901234567890n));
+
+alert(typeof(alert))
+
+let result=prompt("Вопрос","Ответ");
+
+
+let name=prompt("Введите имя","Ответ");
+alert(`Привет ${name}`);
+alert(typeof(name));
+
+let name2= confirm("Твоё имя Иван");
+alert(`Привет ${name2}`);
+alert(typeof(name2))
+
+let name3=prompt("Введите имя");
+let age3=prompt("Введите возраст");
+let pol3=prompt("Введите пол");
+alert(`Привет ${name3},${age3},${pol3}`)
+alert(`{name3,age3},${pol3}`)
+
+let value=true;
+alert(typeof value);
+value=String(value);
+alert(typeof value);
+
+alert(5%2);
+
+alert(4%2)
+
+
+let name='aboba'+'neaboba'
+alert(name);
+alert(typeof(name));
+
+let name1=("1");
+name1=+name1
+
+alert(typeof(name1));
+alert(name1)
+
+let name2='1'
+name2=+name2;
+alert(+name2);
+alert(typeof(name2))
+
+let x=1;
+x=-x;
+alert(x)
+
+let x1=1,y1=3;
+alert(y1-x1)
+
+let x2=3**2;
+alert(x2)
+
+let s='склееная' +' строка'
+alert(s)
+
+let x3="1";
+x3=+x3;
+alert(+x3)
+alert(typeof(X3))
+
+
+let a=1;
+let b=2;
+let c=3-(a=b+1)
+alert(a);
+alert(c);
+
+let counter=1;
+let a1=++counter;
+alert(a1);
+alert(counter)
+
+let counter2=1;
+let a2=counter2++;
+alert(a2);
+alert(counter2)
+
+let name=6;
+alert(++name)
+
+let num=6;
+let num1=num++;
+alert(num1);
+alert(num);
+
+let counter3=1;
+let a4=++counter3;
+alert(a4);
+
+let counter1=1;
+let a3=counter1++;
+alert(a3);
+
+let name1=7;
+alert(name1++);
+
+let name2=7;
+alert(++name2)
+
+
+let name=6;
+let name1=name++;
+alert(name1+10);
+alert(name);
+
+let name2=6;
+let name3=++name2;
+alert(name3+10);
+alert(name2);
+
+let num=6;
+let num1="6";
+alert(num1==num);
+
+alert(0==false);
+
+alert(0===false);
+
+alert(null==undefined);
+
+alert(null===undefined);
+
+let a=1;
+let b=1;
+let c=++a;
+let d=b++;
+alert(a);
+alert(b);
+alert(c);
+alert(d)
+
+let a=2;
+let x=1+(a*=2)
+alert(a);
+alert(x)
+
+let a="";
+let b=1;
+let c=a+b+0
+alert(c)
+
+let a="";
+let b=1;
+let c=a+b-0
+alert(c)
+
+let a=true;
+let b=false;
+let c=a+b;
+alert(c)
+
+let a=true;
+let b=true;
+let c=a+b;
+alert(c)
+
+let a=false;
+let b=false;
+let c=a+b;
+alert(c)
+
+let a=6;
+let b="3"
+let c=a/b;
+alert(c)
+
+let a=4;
+let b="px"
+let c=a+b+5
+alert(c)
+
+let a="$";
+let c=a+4+5;
+alert(c)
+
+let d="4"-2
+alert(d)
+
+let e=7/0;
+alert(e)
+
+let f=" -9  "+5;
+alert(f)
+
+let g="  -9  "-5;
+alert(g)
+
+let a1="1"+1;
+alert(a1)
+
+let j="1"*1;
+alert(j)
+
+let k=""-1+0;
+alert(k)
+
+
+alert(0.5+0.1==0.6)
+
+alert(0.2+0.1==0.3)
+
+alert(0.2+0.1===0.3)
+
+alert('2'>1);
+alert('01'==1);
+alert(true==1);
+alert(true==0);
+
+
+
+let num;
+let num1=null;
+alert (num1==num);
+
+ let year=prompt('В каком году начался COVID?');
+ if(year==2019) alert('Вы правы')
+
+ let year1=prompt();
+ if (year1==2019){alert("yes")}
+ else{alert("no");}
+
+let year2=prompt();
+ if (year2==2019){alert("yes")}
+ else if(year2<2019){alert("rano")}
+ else{alert("no");}
+
+ let num2=prompt();
+ let res=num2==2019?alert("yes"):alert("no");
+
+ let age=prompt("Возраст",18);
+let message=(age<3)?"Здраствуйте, малыш!":
+ (age<18)?"Привет!":
+ (age<100)?"Здравствуйте!":"Какой необычный возраст"
+ alert(message)
+
+ let age=prompt()
+
+ if (age<3){
+    message='Hello, baby'
+ }
+ else if(age<18){
+ message="Hello friend!"
+ }
+ else if(age<100){
+    message="Good day"
+ }
+ else{ 
+    message="Its wunder"
+ }
+ alert(message)
+
+ let a=prompt("ok")
+ let asd=(a==24)?alert("yes"):alert("no");
+ alert(asd)
+
+ et year=prompt("COVID, HOW YEAR")
+ if (year==2019)alert("ok")
+ else alert('no')
+
+ let year=prompt("COVID, HOW YEAR")
+ if (year==2019){alert("ok");alert("all right")}
+ else alert('no')
+
+ if("0"){alert("hello")}
+
+ let year=Number(prompt());
+ alert(typeof(year)
+ 
+ let year1=+prompt()
+ if (year1>0){alert(1)}
+ else if(year1<0){alert(-1)}
+ else{alert(0);}
+ alert(year1)
+
+let a=Number(prompt());
+let b=+prompt();
+let num4=(a+b<4)?"Мало":"Много";
+alert(num4)
+
+let login=prompt("Сообщение");
+let message=(login=='Сотрудник')?"Здраствуйте":
+(login=="Директор")?"Привет":"Dggg";
+alert(message)
+
+let age=+prompt();
+if (14<=age  && age<=90) alert("Включительно");
+else  alert("no number")
+
+
+let age=+prompt();
+if (age<=14 || age>=90){alert("не в диапозоне");}
+else  {alert("no")}
+
+
+
+let age=+prompt();
+if (age>=14 && age<=90){alert("не в диапозоне");}
+else  {alert("no")}
+
+alert(!true);
+alert(!0);
+
+let user=prompt('you')
+
+if (user=='admin'){alert( admin=prompt('password'))}
+else alert('no')
+if (admin=='head'){alert('hello')}
+else alert('not found')*/
+
+
+
+/*let age=+prompt();
+switch(age) {
+    case 1:
+        alert(`Равен ${age}`)
+        break;
+    case 2:
+        alert(`Равен ${age}`)
+        break;
+    default:
+        alert("Это блок дефолт")
+        break;
+}
+
+let age=+prompt();
+switch(age) {
+    case 1:
+    case 2:
+    case 3:
+        alert(`Равен ${age}`)
+        break;
+    case 4:
+        alert(`Равен ${age}`)
+        break;
+    default:
+        alert("Это блок дефолт")
+        break;
+
+
+let age=+prompt();
+switch(age) {
+    case 1:
+    case 2:
+    case 3:
+        alert(`Равен ${9}`)
+        break;
+    case 4:
+        alert(`Равен ${age}`)
+        break;
+    default:
+        alert("Это блок дефолт")
+        break;
+}
+
+let age=+prompt();
+let age1=+prompt();
+let znak=prompt();
+switch(znak) {
+    case "+":
+        alert(age+age1)
+        
+        break;
+    case "-":
+        alert(age-age1)
+        break;
+    case "*":
+        alert(age*age1)
+        break;
+    case "/":
+       
+default:
+    if(age===0 || age1===0){
+        alert("Это блок дефолт")
+}
+else{
+        alert(age/age1);
+    }
+        break;
+}
+------------------------------------------------------------
+let A=+prompt();
+let B=+prompt();
+
+
+switch(A-B>0){
+    case true:
+        alert(A);
+        break;
+    case false:
+        alert(B);
+        break;
+    
+   
+    default:
+        if ((A-B)===0){
+        alert(0)}
+ 
+        break;
+}
+-------------------------------------------------------
+let name=prompt();
+let count=0;
+while (count>5)
+{console.log(name);
+count++;}
+------------------------------------------------------
+
+let name=prompt();
+let count=0;
+do{
+    console.log(name);
+    count++;
+}
+while (count<5);
+-------------------------------------------------------------
+
+for(let i=0;i<10;i++){
+ console.log(i)}
+---------------------------------------------------------
+
+for(let i=0;i<10;i++){
+ console.log(i)
+}
+
+-------------------------------------------------------------
+
+ let summa=0;
+ for(let i=0;i<10;i++){
+    if (i%2==0);{
+    console.log(i)
+    summa+=i;}
+ }
+ console.log(summa);
+ 
+ ------------------------------------------
+
+ let sum = 0;
+ while (true) {
+	 let value = +prompt("Введите число", "");
+	 if (!value) break; 
+	sum += value; }
+ alert( 'Сумма: ' + sum );
+
+
+ let summa=1;
+ for(let i=1;i<11;i++){   
+    summa*=i;}
+ console.log(summa);
+
+
+
+let num;
+do{
+    let number=Number(prompt("Enter num"));
+    if (number===null){
+        break;
+    }
+    num=Number(number)
+    if (num<=100){
+        alert("eshe");
+
+
+    }
+    else{
+        alert("all right");
+        break;
+    }
+
+}while(num<=100)
+  
+
+let num=prompt();
+let res=""
+for (let i=0;i<7;i++){
+    let row="";
+    for(let j=0;j<=20;j++){
+            row+=num;
+    }
+    row+="\n"
+    res+=row
+}
+    console.log(res);
+    document.write(res);
+    
+
+    for (let i = 0; i < 10; i++) {
+	  if (i % 2 == 0) continue; 
+	
+
+  	alert(i); 
+}
+
+  
+ let nam=prompt('Введи имя')
+ let age=prompt('Введи возраст')
+ let number=prompt('Введи номер телефона')
+ 
+ function login(nam, age, number){
+     if(nam==null){
+         console.log('так не подходит')
+
+     }
+     else{
+         console.log('ок')
+     }
+     if (age>=18){
+         console.log("Вы совершеннолетний")
+    }
+    else if(age==null){
+       console.log('так не подходит')
+
+    }
+    else{
+         console.log('Так не подходит')
+     }
+     if (number.length=11){
+         console.log('Корректный номер')
+     }
+     else if(number==null){
+         console.log('так не подходит')
+
+   }
+     else{
+         console.log('Некорректный номер')
+     }
+
+ }
+ a=login(nam,age,number)
+ console.log(a)
+
+ 
+ 
+ function login(nam, age, number) {
+     if (nam === null) {
+         console.log('так не подходит');
+     } else {
+
+         console.log('ок');
+     }
+
+     if (age === null) {
+         console.log('так не подходит');
+     } else if (age >= 18) {
+         console.log('Вы совершеннолетний');
+     } else {
+         console.log('Так не подходит');
+     }
+
+    if (number === null || number.length !== 11) {
+         console.log('Некорректный номер');
+     } else {
+         console.log('Корректный номер');
+     }
+ }
+
+ let nam = prompt('Введи имя');
+ let age = +prompt('Введи возраст');
+ let number = prompt('Введи номер телефона'); 
+ login(nam, age, number);
+
+ -----------------------------------------------------------------------------------------------------------------------------
+
+ let a=Number(prompt());
+ let b=Number(prompt());
+
+ function summ(a,b){
+     return a+b
+  }
+
+ let rez=summ(a,b);
+ console.log(rez);
+
+
+
+
+
+ let a=Number(prompt());
+ let b=Number(prompt());
+
+ function summ(a,b){
+     return a**b
+  }
+
+ let rez=summ(a,b);
+ console.log(rez);
+
+ let a=Number(prompt());
+ let b=Number(prompt());
+
+ function rezultat(a,b){
+
+     if (a%2===0 && b%2===0){
+         return a*b
+     }
+
+     if (a%2===0 && b%2!=0){
+         return b
+     }
+
+     if (a%2!=0 && b%2!=0){
+         return a+b
+     }
+
+     if (a%2!=0 && b%2===0){
+         return b
+     }
+
+ }
+
+ let rez=rezultat(a,b);
+
+ console.log(rez);
+
+
+
+
+ let a=Number(prompt());
+ let b=Number(prompt());
+ let c=(prompt());
+
+ function rezultat(a,b,c){
+
+     if (c=="+"){
+         return a+b;
+     }
+
+     else if (c=="-"){
+         return a-b;
+     }
+
+     else if (c=="*"){
+         return a*b;
+     }
+
+     else if(c=="/"){
+         return a/b;
+     }
+ }
+
+ let rez=rezultat(a,b,c);
+ alert(rez);
+
+
+
+ function ask(quest, yes, no) {
+    if (confirm(quest)) yes()
+    else no();
+ }
+
+ function ok() {
+    alert( "Вы согласны." );
+ }
+
+ function nah() {
+    alert( "Вы отменили выполнение." );
+ }
+
+ ask("Вы согласны?", ok, nah);
+
+
+ let a=Number(prompt());
+ let b=Number(prompt());
+ let sum = (a, b) => a + b; 
+ let ent=sum(a,b)
+ alert(ent)
+
+
+
+
+passsword=prompt('Password');
+
+ if (passsword.length>5 && passsword.length<11){
+     alert('Пароль соответствует заданным условиям')
+ }
+ else{
+     alert('Пароль не соответствует заданным условиям')
+ }
+
+
+
+
+
+ let fruits = ["Яблоко", "Апельсин", "Слива"];
+ fruits[125]=0;
+ console. log(fruits);
+
+ console.log(fruits.length);
+ fruits.length=0
+ console.log(fruits)
+
+
+
+
+function sumUserInput() {
+    let sum = 0;
+
+    while (true) {
+        let input = prompt('Введите число 
+        (оставьте пустым или нажмите Отмена для завершения):');
+        if (input === null) {
+            break;
+        }
+        let num = Number(input);
+        if (isNaN(num)) {
+            console.log('Вы ввели не число. Завершение.');
+            break;
+        }
+        sum += num;
+    }
+    console.log('Сумма:', sum);
+}
+sumUserInput();
+
+
+
+
+let user={
+    name:"Aboba",
+    age:30,
+    phone:445555
+};
+console.log(user.name,user.age,user.phone);
+user.email="aboba@mail.ru";
+user["Aboba ban"]="Ban"
+
+console.log(user);
+
+
+
+
+let fruit=prompt();
+let bag={};
+bag[fruit]=5;
+console.log(bag);
+
+
+
+let fruit=prompt();
+let bag={};
+bag[fruit]=5;
+console.log(bag);
+alert("Aboba" in bag);
+
+
+
+let user={
+    name:"Aboba",
+    age:30,
+    phone:445555
+};
+console.log(user.name,user.age,user.phone);
+user.email="aboba@mail.ru";
+user["Aboba ban"]="Ban"
+for (key in user){
+    console.log(key);
+    console.log(user[key]);
+}
+
+
+let name=prompt();
+let user={};
+user[name]="John";
+user.surname="Smith"
+user[name]="Piter";
+delete user.name;
+console.log(user);
+
+
+
+
+let user={
+    name1:5666677,
+    name2:344545,
+    name3:554555
+    
+};
+let sum=0;
+for (let key in user){;
+sum+=user[key]};
+console.log(sum);
+
+
+
+function mult(obj){
+      for ( let key in obj){
+    if (typeof obj[key]==="number"){
+            obj[key]*=2;
+        }
+    }    
+}
+let object={
+    num1:2,
+    num2:3,
+    num3:4
+};
+mult(object);
+console.log(object)
+
+
+for (let node of document.body.childNodes){
+    alert(node);
+}
+
+  
+
+
+for (let node of document.head.childNodes){
+    alert(node);
+}
+
+
+
+
+for (let node of document.head.nextSibling){
+    alert(node);
+}
+
+
+
+for (let node of document.head.previousSibling){
+    alert(node);
+}
+
+
+let elem=document.getElementById("elem");
+elem.style.background="red";
+elem.style.width="50%"
+elem.style.height="120px"
+elem.style.color="green"
+elem.style.border="yellow,5px"
+elem.style.textShadow="45px"
+
+
+let elem=document.getElementById("elem");
+let elem1=document.getElementsByTagName("div");
+console.log(elem1)
+elem.style.background="red";
+elem.style.heidth="100px";
+elem1.style.width="50%";
+elem1.style.background="green"
+
+
+
+let elem=document.getElementById("elem");
+let elem1=document.getElementsById("elem-content");
+console.log(elem1)
+elem.style.background="red";
+elem.style.heidth="500px";
+elem1.style.width="50%";
+elem1.style.background="green"
+
+
+
+let elem=document.querySelector("#elem");
+let elem1=document.getElementById("elem-content");
+console.log(elem)
+let spisok=document.querySelector("ul>li:last-child");
+console.log(spisok)
+for (let elem of spisok)
+{
+    console.log(elem.innerHTML);
+}
+
+
+
+let elem=document.querySelector("#elem");
+
+let elem=document.querySelector(".elem");
+let elem=document.querySelector("div");
+let elem1=document.getElementById("elem-content");
+console.log(elem)
+let spisok=document.querySelector("ul>li:last-child");
+console.log(spisok)
+for (let elem of spisok)
+{
+    console.log(elem.innerHTML="Ban");
+}
+    
+    
+
+
+
+let spisok=document.querySelector("ul>label:last-child");
+console.log(spisok)
+for (let elem of spisok)
+{
+    console.log(elem.innerHTML);}
+
+
+
+
+let spisok=document.querySelector("form>input:last-child");
+console.log(spisok)
+for (let elem of spisok)
+{
+    console.log(elem.innerHTML);}
+console.dir(spisok);
+
+
+let body=document.body;
+body.innerHTML="<!--"+"OOOO"+"-->"
+alert(body.firstChild.data);
+
+
+let elem=document.getElementById("elem");
+console.log(elem.aboba);
+
+let elem=document.getElementById
+
+
+
+function showMessage(){
+    let message=" Привет , я JavaScript";
+    alert (message);
+}
+showMessage();
+alert (message);
+
+
+
+let userName='Вася';
+function showMessage(){
+    let message='Привет,'+ userName;
+    alert (message);
+}
+showMessage();
+
+
+
+function showMessage(name,message){
+    alert(name +':'+message);
+}
+showMessage('Аня'," Привет!")
+showMessage('Вова'," Как дела?")
+
+
+
+function showMessage(name,message="Привет"){
+    alert(name+':'+message);
+}
+showMessage("Аня");
+showMessage("Вова","Как дела?");
+
+
+function showMessage(name,message=deffunction()){
+    alert(name+':'+message);
+}
+function deffunction(){
+    return "Привет!";
+}
+showMessage("Аня");
+showMessage('Вова',"Как,дела?");
+
+
+function showMessage(from,text){
+    if (text==undefined){
+        text='текст не добавлен';
+    }
+    alert (from +":"+text);
+}
+showMessage();
+
+
+function showMessage(from,text){
+    if (text==undefined){
+        text='текст не добавлен';
+    }
+    alert (from +":"+text);
+}
+showMessage("hello","champion");
+
+
+function  summa(argument1,argument2){
+    return argument1+argument2;
+}
+console.log('Сумма чисел равна');
+let sum=summa(2,3);
+console.log(sum);
+
+
+
+return(
+    some+long+expression
+    +or+
+    whatever*f(a)+f(b)
+)
+
+
+function sayHi(){
+    alert("Привет");
+}
+sayHi();
+
+
+let sayHi=function(){
+    alert("Привет");
+};
+sayHi();
+
+
+function sayHi(){
+    alert("Привет");
+}
+alert(sayHi);
+
+
+function ask(question,yes,no){
+    if (confirm(question))yes()
+        else no();
+}
+function showOk(){
+    alert("Вы согласны");
+}
+function showCancel(){
+    alert("Вы отменил выполнение");
+}
+ask ("Вы согласны?",showOk,showCancel);
+
+
+
+let func=(arg1,arg2,...argN);
+let sum=(a,b)=>a+b
+alert(sum(1,2))
+
+
+let a=Number(prompt());
+ let b=Number(prompt());
+ let sum = (a, b) => a + b; 
+ let ent=sum(a,b)
+ alert(ent)
+ 
+
+ let sum=function(a,b){
+    return a+b;
+ }
+ alert(sum(1,2));
+
+ 
+
+ let double=n=>n*2;
+ alert(double(3));
+ 
+let sum=(a,b)=>{
+    let result=a+b;
+    return result;
+};
+alert(sum(1,2));
+ 
